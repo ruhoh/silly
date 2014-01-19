14d55388f5581feb7523720e6373985ed04867cf
Jade Dominguez <plusjade@gmail.com>
2014-01-19 01:16:25 -0800

    Add data and content parsing hooks to execute callbacks.
    This allows the implementation to execute arbitrary data transformations as before/after callbacks.

--- lib/silly/page_model.rb
+++ lib/silly/page_model.rb
@@ -3,21 +3,39 @@ module Silly
     DateMatcher = /^(.+\/)*(\d+-\d+-\d+)-(.*)(\.[^.]+)$/
     Matcher = /^(.+\/)*(.*)(\.[^.]+)$/
 
+    class << self
+      attr_accessor :before_data, :after_data, :before_content, :after_content
+    end
+
     def data
       return {} unless file?
 
       data = Silly::Parse.page_file(realpath)["data"]
       data['id'] = id
 
+      if self.class.before_data.respond_to?(:call)
+        data = self.class.before_data.call(data)
+      end
+
       filename_data = parse_page_filename(id)
       data['title'] ||= filename_data['title']
       data['date'] = parse_date(data['date'] || filename_data['date'])
 
-      data
+      self.class.after_data.respond_to?(:call) ?
+        self.class.after_data.call(data) :
+        data
     end
 
     def content
-      Silly::Parse.page_file(realpath)["content"]
+      result = Silly::Parse.page_file(realpath)["content"]
+
+      if self.class.before_content.respond_to?(:call)
+        result = self.class.before_content.call(result)
+      end
+
+      self.class.before_content.respond_to?(:call) ?
+        self.class.before_content.call(result) :
+        result
     end
 
     private
